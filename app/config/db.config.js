396f09720d22bd68921bb5421cc7028239e043a1
DanielTan1985 <97973757+DanielTan1985@users.noreply.github.com>
2022-05-10 21:11:24 +0800

    Updated db.config file

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  HOST: "ec2-54-80-122-11.compute-1.amazonaws.com",
-  USER: "bimumvggwjprjc",
-  PASSWORD: "1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf",
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "1234qwer",
   DB: "d7r4o5vvh2upn9",
   dialect: "postgres",
   pool: {
