f8d508e2b0f7ee0df59bef10bd1376a9a23a4d82
DanielTan1985 <97973757+DanielTan1985@users.noreply.github.com>
2022-05-11 15:43:12 +0800

    Added sequelize into db.config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize('d7r4o5vvh2upn9', 'bimumvggwjprjc', '1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf',{
   HOST: "ec2-54-80-122-11.compute-1.amazonaws.com",
   USER: "bimumvggwjprjc",
   PASSWORD: "1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf",
@@ -10,14 +11,25 @@ module.exports = {
     acquire: 30000,
     idle: 10000
   }
-};
+})
+module.exports = {
+  sequelize
+}
 
 
 
 
 
-
-//   const sequelize = new Sequelize('d7r4o5vvh2upn9', 'postgres', '1234qwer',{
-//     host: 'localhost',
-//     dialect: 'postgres',
-// })
+// module.exports = {
+//   HOST: "ec2-54-80-122-11.compute-1.amazonaws.com",
+//   USER: "bimumvggwjprjc",
+//   PASSWORD: "1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf",
+//   DB: "d7r4o5vvh2upn9",
+//   dialect: "postgres",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
