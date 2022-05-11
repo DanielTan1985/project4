7a50127e456cb4854db5876d5d2872197c8e7e09
DanielTan1985 <97973757+DanielTan1985@users.noreply.github.com>
2022-05-11 14:39:03 +0800

    Latest copy of code

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: "./dev.sqlite",
-  USER: "USER",
-  PASSWORD: "PASSWORD",
-  DB: "DB",
-  dialect: "sqlite",
+  HOST: "ec2-54-80-122-11.compute-1.amazonaws.com",
+  USER: "bimumvggwjprjc",
+  PASSWORD: "1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf",
+  DB: "d7r4o5vvh2upn9",
+  dialect: "postgres",
   pool: {
     max: 5,
     min: 0,
@@ -11,3 +11,18 @@ module.exports = {
     idle: 10000
   }
 };
+
+
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "postgres",
+//   PASSWORD: "1234qwer",
+//   DB: "d7r4o5vvh2upn9",
+//   dialect: "postgres",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
