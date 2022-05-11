4c608d790b67a62ed518b1f219071c8dc00b0c61
DanielTan1985 <97973757+DanielTan1985@users.noreply.github.com>
2022-05-11 12:55:57 +0800

    1

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,27 @@
+const sequelize = new Sequelize('d7r4o5vvh2upn9', 'postgres', '1234qwer',{
+    host: 'localhost',
+    dialect: 'postgres',
+    
+    
+})
+
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "1234qwer",
-  DB: "d7r4o5vvh2upn9",
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
+  sequelize,
+}
+
+
+
+
+
+
+  // HOST: "localhost",
+  // USER: "postgres",
+  // PASSWORD: "1234qwer",
+  // DB: "d7r4o5vvh2upn9",
+  // dialect: "postgres",
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000
+  // }
