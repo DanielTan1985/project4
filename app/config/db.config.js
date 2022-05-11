01f10770d2481072bc9e1ef83af44db04afd5c6a
DanielTan1985 <97973757+DanielTan1985@users.noreply.github.com>
2022-05-11 15:35:57 +0800

    edit db.confg2

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
-const sequelize = new Sequelize('d7r4o5vvh2upn9', 'postgres', '1234qwer',{
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "1234qwer",
+module.exports = {
+  HOST: "ec2-54-80-122-11.compute-1.amazonaws.com",
+  USER: "bimumvggwjprjc",
+  PASSWORD: "1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf",
   DB: "d7r4o5vvh2upn9",
   dialect: "postgres",
   pool: {
@@ -10,12 +10,7 @@ const sequelize = new Sequelize('d7r4o5vvh2upn9', 'postgres', '1234qwer',{
     acquire: 30000,
     idle: 10000
   }
-})
-
-module.exports = {
-  sequelize
-}
-
+};
 
 
 
