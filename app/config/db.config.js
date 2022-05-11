d75c2975d46a3b4cf8bd4312c63eaf1d3e9d94ca
DanielTan1985 <97973757+DanielTan1985@users.noreply.github.com>
2022-05-11 15:51:16 +0800

    switch dbconfig

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,26 +1,5 @@
-const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize('d7r4o5vvh2upn9', 'bimumvggwjprjc', '1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf',{
-  HOST: "ec2-54-80-122-11.compute-1.amazonaws.com",
-  USER: "bimumvggwjprjc",
-  PASSWORD: "1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf",
-  DB: "d7r4o5vvh2upn9",
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-})
-module.exports = {
-  sequelize
-}
-
-
-
-//testing
-
-// module.exports = {
+// const { Sequelize } = require("sequelize");
+// const sequelize = new Sequelize('d7r4o5vvh2upn9', 'bimumvggwjprjc', '1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf',{
 //   HOST: "ec2-54-80-122-11.compute-1.amazonaws.com",
 //   USER: "bimumvggwjprjc",
 //   PASSWORD: "1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf",
@@ -32,4 +11,25 @@ module.exports = {
 //     acquire: 30000,
 //     idle: 10000
 //   }
-// };
+// })
+// module.exports = {
+//   sequelize
+// }
+
+
+
+//testing
+
+module.exports = {
+  HOST: "ec2-54-80-122-11.compute-1.amazonaws.com",
+  USER: "bimumvggwjprjc",
+  PASSWORD: "1d670fcd93a18be2cefeae429e845731e01788cd626b2e8382ec7798467bf7cf",
+  DB: "d7r4o5vvh2upn9",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
