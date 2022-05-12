f976099b59ff816ad418d7b088dcd82509aa4fb1
DanielTan1985 <97973757+DanielTan1985@users.noreply.github.com>
2022-05-12 17:14:16 +0800

    Update index.js in routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,10 +5,10 @@ app.use(express.json()); // Enable express to parse JSON as request body.
 
 const userRoutes = require("./user.routes");
 const publicRoutes = require("./public.routes");
-const loginRoutes = require('./login.routes');
+const authRoutes = require('./auth.routes');
 
 app.use(userRoutes);
 app.use(publicRoutes);
-app.use(loginRoutes);
+app.use(authRoutes);
 
 module.exports = app;
